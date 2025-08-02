f45edfc73f2678ed97597daf679078a0f9b8fa7d
Your Name <you@example.com>
2025-08-02 15:14:54 +0900

    test

--- src/main/java/com/gigatomson/GigaTomsonApplication.java
+++ src/main/java/com/gigatomson/GigaTomsonApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GigaTomsonApplication {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(GigaTomsonApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
