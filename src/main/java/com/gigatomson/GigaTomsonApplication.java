a1b905e74ea852e0ea9586ed0aa11b96c2561f43
Your Name <you@example.com>
2025-08-02 14:55:38 +0900

    ci/cd용 파일

---
+++ src/main/java/com/gigatomson/GigaTomsonApplication.java
@@ -0,0 +1,11 @@
+package com.gigatomson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigaTomsonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GigaTomsonApplication.class, args);
+    }
+}
---
+++ src/main/java/com/gigatomson/TestController.java
@@ -0,0 +1,15 @@
+package com.gigatomson;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "CI/CD 성공!!!!!!!!!!!";
+    }
+
+}
