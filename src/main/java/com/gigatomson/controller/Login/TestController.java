1d30ba5b39a99011ef996fc3774aa06efdbf4f8f
Your Name <you@example.com>
2025-08-03 19:23:04 +0900

    ci/cd 테스트2

--- src/main/java/com/gigatomson/controller/Login/TestController.java
+++ src/main/java/com/gigatomson/controller/Login/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
 
     @GetMapping("/")
     public String hello() {
-        return "CI/CD 성공 2025/08/03 tlqkf... 7시간걸렸다...";
+        return "CI/CD 성공 2025/08/03 tlqkf... 7시간걸렸다... 추가로 이미지들 최근꺼 2개 제외하고 빌드될때마다 정리(용량 관리)";
     }
 
 }
